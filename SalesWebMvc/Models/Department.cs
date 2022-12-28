6e943f63b0e67b2422b7ad5af348fec106acc78c
SamuelPj <samueljacobsen@alunos.utfpr.edu.br>
2022-12-28 16:18:57 -0300

    SeedingServices

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System;
 
 namespace SalesWebMvc.Models
 {
+    [Table("Department")]
     public class Department
     {
+        [Display(Name = "Código")]
+        [Column("Id")]
         public int Id { get; set; }
-        public int Name { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Name { get; set; }
+
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public Double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(Seller => Seller.TotalSales(initial, final));
+        }
     }
 }
