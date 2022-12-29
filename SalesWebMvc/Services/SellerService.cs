2c963c1a2e146cd2e475de7b00c0c692a3caac56
SamuelPj <samueljacobsen@alunos.utfpr.edu.br>
2022-12-29 16:49:45 -0300

    Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace SalesWebMvc.Services
 {
     public class SellerService
@@ -19,10 +18,20 @@ namespace SalesWebMvc.Services
         }
         public void Insert(Seller obj)
         {
-          
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
 }
-
